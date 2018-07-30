ea9ac6c8f29fab8fe775c984767e80784a00e30f
Dmitry Zhelnin <dmitry.zhelnin@gmail.com>
2018-07-30 19:09:24 +0300

    update abci for latest tendermint version

--- config/config.exs
+++ config/config.exs
@@ -30,5 +30,5 @@ use Mix.Config
 #     import_config "#{Mix.env}.exs"
 
 config :pallium,
-  host: "http://localhost:46657/",
+  host: "http://localhost:26657/",
   broadcast: "broadcast_tx_commit?tx="
