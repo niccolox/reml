64632c6e3e8d39c82588a2a7b4b222a803125dbf
Dmitry Zhelnin <dmitry.zhelnin@gmail.com>
2018-08-30 01:22:51 +0300

    do not start Store in Pallium.start

--- lib/pallium.ex
+++ lib/pallium.ex
@@ -4,16 +4,11 @@ defmodule Pallium do
   """
   alias Pallium.Api.Server
   alias Pallium.App
-  alias Pallium.Core.Store
-
-  alias MerklePatriciaTree.Test
-  alias MerklePatriciaTree.Trie
 
   @type key :: binary
   @type address :: <<_::160>>
 
   def start do
-    Test.random_ets_db() |> Trie.new() |> Store.start_link()
     App.init()
     Server.start_link()
   end
