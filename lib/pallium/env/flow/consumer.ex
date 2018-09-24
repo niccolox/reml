c2a1603fa2794016ea313e2c4b5617b3ef1a81e7
Anton Zhuravlev <froggrog@hotmail.com>
2018-09-24 14:44:41 +0300

    new agents concept

--- lib/pallium/env/flow/consumer.ex
+++ lib/pallium/env/flow/consumer.ex
@@ -1,18 +1,37 @@
 defmodule Pallium.Env.Flow.Consumer do
   @moduledoc false
   use GenStage
+  
+  alias Pallium.Env.Flow.Producer
 
   def start_link(size) do
-    GenStage.start_link(__MODULE__, %{size: size, producers: []})
+    GenStage.start_link(__MODULE__, %{size: size, producers: [], sum: []})
   end
   
   def init(state) do
    {:consumer, state}
   end
 
-  def handle_events(events, from, state) do
-    IO.inspect from
-    IO.inspect events
-    {:noreply, [], state}
+  def handle_events([:hello], from, state) do
+    {pid, _} = from 
+    IO.puts("Hello i'am: #{inspect length(state.producers)}")
+    next_state = %{state | producers: state.producers ++ [pid]}
+    if length(state.producers) == state.size do
+      Enum.each(state.producers, fn p -> Producer.add(p, 2, 2) end)
+    end 
+    {:noreply, [], next_state}
+  end
+
+  def handle_events([[add: result]], _from, state) do
+    IO.puts("Event result: #{result}")
+    next_state = %{state | sum: state.sum ++ [result]}
+    IO.puts("Event sum: #{inspect next_state.sum}")
+
+    if length(next_state.sum) == state.size do
+      sum = Enum.reduce(next_state.sum, 0, fn x, acc -> x + acc end)
+      IO.puts("Reduce result: #{sum}")
+    end
+    
+    {:noreply, [], next_state}
   end
 end
--- test/env/flow_test.exs
+++ test/env/flow_test.exs
@@ -1,19 +1,14 @@
-defmodule WrapperTest do
+defmodule FlowTest do
   use ExUnit.Case
 
-  alias Pallium.Env.Flow.{Wrapper, Consumer}
+  alias Pallium.Env.Flow.{Producer, Consumer}
  
   test "connected" do
-    task = fn input -> 
-      rand = :rand.uniform(1000)
-      Process.sleep(rand) 
-      rand
-    end
     size = 4
     {:ok, c} = Consumer.start_link(size)
 
     Enum.each(0..size, fn _ -> 
-      {:ok, w} = Wrapper.start_link(task)
+      {:ok, w} = Producer.start_link()
       GenStage.sync_subscribe(c, to: w)
     end)
 
